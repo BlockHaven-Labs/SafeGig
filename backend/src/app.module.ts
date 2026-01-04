adf77a0eb83c0b301fc1154fe5e22d70d715c8fd
Glen <glendmello04@gmail.com>
2026-01-04 19:12:18 +0530

    chore(modules): add placeholder modules under src/modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { UsersModule } from 'modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { JobsModule } from './modules/jobs/jobs.module';
+import { EscrowModule } from './modules/escrow/escrow.module';
+import { FilesModule } from './modules/files/files.module';
+import { MessagingModule } from './modules/messaging/messaging.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 
 @Module({
   imports: [
@@ -12,6 +18,12 @@ import { ConfigModule } from '@nestjs/config';
     }),
     DatabaseModule,
     UsersModule,
+    AuthModule,
+    JobsModule,
+    EscrowModule,
+    FilesModule,
+    MessagingModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
